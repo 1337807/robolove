66f9b709b2a406c616d1f275d4c12a146e8e06ae
Jonan Scheffler <jonanscheffler@gmail.com>
2015-01-15 10:31:05 -0800

    Default options and left turns.

--- lib/robolove/bot.rb
+++ lib/robolove/bot.rb
@@ -2,24 +2,39 @@ require 'lego_nxt'
 
 module Robolove
   class Bot
-    attr_reader :brick
+    DEFAULTS = {
+      :brick => nil,
+      :left_motor => :a,
+      :right_motor => :b
+    }
+    attr_reader :brick, :left_motor, :right_motor
 
-    def initialize(brick = nil)
-      @brick = brick || LegoNXT::LowLevel.connect
+    def initialize(options = {})
+      options = DEFAULTS.merge options
+
+      @brick = options[:brick] || LegoNXT::LowLevel.connect
+      @left_motor = options[:left_motor] || DEFAULTS[:left_motor]
+      @right_motor = options[:right_motor] || DEFAULTS[:right_motor]
     end
 
     def forward(duration = 1)
-      self.brick.run_motor(:a)
-      self.brick.run_motor(:b)
+      self.brick.run_motor(self.left_motor)
+      self.brick.run_motor(self.right_motor)
       sleep duration
-      self.brick.stop_motor(:a)
-      self.brick.stop_motor(:b)
+      self.brick.stop_motor(self.left_motor)
+      self.brick.stop_motor(self.right_motor)
     end
 
     def right(duration = 0.5)
-      self.brick.run_motor(:a)
+      self.brick.run_motor(self.left_motor)
       sleep duration
-      self.brick.stop_motor(:a)
+      self.brick.stop_motor(self.left_motor)
+    end
+
+    def left(duration = 0.5)
+      self.brick.run_motor(self.right_motor)
+      sleep duration
+      self.brick.stop_motor(self.right_motor)
     end
   end
 end
--- test/bot_test.rb
+++ test/bot_test.rb
@@ -3,46 +3,64 @@ require 'test_helper'
 class BotTest < Minitest::Test
   def setup
     @brick = mock
-    @bot = Robolove::Bot.new(@brick)
+    @left = 'test_left'
+    @right = 'test_right'
+    @bot = Robolove::Bot.new(brick: @brick, left_motor: @left, right_motor: @right)
   end
 
   def test_bot_creates_a_brick
     brick = 'fake brick'
     ::LegoNXT::LowLevel.stub(:connect, brick) {}
-    bot = Robolove::Bot.new(brick)
+    bot = Robolove::Bot.new(brick: brick)
     assert_equal brick, bot.brick
   end
 
   def test_forward_runs_both_motors
     def @brick.stop_motor(port); true; end
-    @brick.expect(:run_motor, true, [:a])
-    @brick.expect(:run_motor, true, [:b])
+    @brick.expect(:run_motor, true, [@left])
+    @brick.expect(:run_motor, true, [@right])
     @bot.forward
     @brick.verify
   end
 
   def test_forward_stops_motors_after_duration
     def @brick.run_motor(port); true; end
-    @brick.expect(:stop_motor, true, [:a])
-    @brick.expect(:stop_motor, true, [:b])
+    @brick.expect(:stop_motor, true, [@left])
+    @brick.expect(:stop_motor, true, [@right])
 
     @bot.forward(0)
     @brick.verify
   end
 
-  def test_right_runs_motor_a
+  def test_right_runs_motor
     def @brick.stop_motor(port); true; end
-    @brick.expect(:run_motor, true, [:a])
+    @brick.expect(:run_motor, true, [@left])
 
     @bot.right
     @brick.verify
   end
 
-  def test_right_stops_motor_a_after_duration
+  def test_right_stops_motor_after_duration
     def @brick.run_motor(port); true; end
-    @brick.expect(:stop_motor, true, [:a])
+    @brick.expect(:stop_motor, true, [@left])
 
     @bot.right(0)
+    @brick.verify
+  end
+
+  def test_left_runs_motor
+    def @brick.stop_motor(port); true; end
+    @brick.expect(:run_motor, true, [@right])
+
+    @bot.left
+    @brick.verify
+  end
+
+  def test_right_stops_motor_after_duration
+    def @brick.run_motor(port); true; end
+    @brick.expect(:stop_motor, true, [@right])
+
+    @bot.left(0)
     @brick.verify
   end
 end
